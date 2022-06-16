a89e0c0f9c9ed2b8026baed564b8d5944d087c92
gerard-moledo <gerard.moledo@gmail.com>
2022-06-15 21:10:59 -0400

    Add movement state to player

--- include/player.hpp
+++ include/player.hpp
@@ -4,14 +4,25 @@
 #include "glm/glm.hpp"
 
 #include "models.hpp"
+#include "utils.hpp"
+
+enum class State { idle, moving };
 
 struct Player
 {
     Player(Renderer& renderer);
 
+    State state = State::idle;
+    Vector2i currentPos;
+    Vector2i targetPos;
+
     CubeModel cube;
 
+    void Update();
     void Draw(glm::mat4 renderTransform);
+
+private:
+    void UpdateModel();
 };
 
 #endif
--- src/player.cpp
+++ src/player.cpp
@@ -1,12 +1,47 @@
 #include "player.hpp"
 
+#include "glfw/glfw3.h"
+#include "glm/gtc/matrix_transform.hpp"
+
+#include "system.hpp"
+
 Player::Player(Renderer& renderer) :
-    cube(renderer)
+    cube(renderer),
+    currentPos({ 0, 0 }),
+    targetPos(currentPos)
 {
     
 }
 
+void Player::Update()
+{
+    if (state == State::idle)
+    {
+        if (glfwGetKey(System::window, GLFW_KEY_D) == GLFW_PRESS)
+        {
+            targetPos.x += 1;
+            state = State::moving;
+        }
+    }
+    if (state == State::moving)
+    {
+        if (glfwGetKey(System::window, GLFW_KEY_SPACE) == GLFW_PRESS)
+        {
+            currentPos.x = targetPos.x;
+            state = State::idle;
+        }
+    }
+}
+
 void Player::Draw(glm::mat4 renderTransform)
 {
+    UpdateModel();
+    
     cube.Draw(renderTransform);
+}
+
+void Player::UpdateModel()
+{
+    cube.model = glm::mat4(1.0f);
+    cube.model = glm::translate(cube.model, glm::vec3(currentPos.x * 2.0f, 0.0f, currentPos.y * -2.0f));
 }
