c0f6d46dbea9b17db159c4cb0f45b62ed9936f92
gerard-moledo <gerard.moledo@gmail.com>
2022-06-18 07:21:22 -0400

    Add 'block' include guard, type enum, and state typedef

--- include/block.hpp
+++ include/block.hpp
@@ -1,9 +1,16 @@
+#ifndef BLOCK_H
+#define BLOCK_H
+
 #include "glm/glm.hpp"
 
 #include "models.hpp"
 #include "renderer.hpp"
 #include "utils.hpp"
 
+enum class BlockType { wall, block, goal };
+
+typedef std::pair<BlockType, Vector2i> BlockState;
+typedef std::vector<BlockState> BlockStates;
 
 struct Block
 {
@@ -17,9 +24,13 @@ struct Block
     glm::vec3 worldPos;
     
     float tMove = 0.0f;
+
+    BlockType type = BlockType::wall;
     
     CubeModel cube;
 
     void Update(float dt);
     void Draw(glm::mat4 renderTransform, float lag);
 };
+
+#endif
