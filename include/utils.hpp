d9946ec68554b35072c7d69982da0a601be39e41
gerard-moledo <gerard.moledo@gmail.com>
2022-06-18 00:13:23 -0400

    Add Vector2i operations (+,-,=,==,!=)

--- include/utils.hpp
+++ include/utils.hpp
@@ -5,6 +5,30 @@ struct Vector2i
 {
     int x;
     int y;
+
+    Vector2i operator+(const Vector2i& other)
+    {
+        return Vector2i { x + other.x, y + other.y };
+    }
+    Vector2i operator-(const Vector2i& other)
+    {
+        return Vector2i { x - other.x, y - other.y };
+    }
+    Vector2i& operator=(const Vector2i& other)
+    {
+        x = other.x;
+        y = other.y;
+        return *this;
+    }
+    bool operator==(const Vector2i& other)
+    {
+        return x == other.x && y == other.y;
+    }
+    bool operator!=(const Vector2i& other)
+    {
+        return x != other.x || y != other.y;
+    }
+    
 };
 
 #endif
